6369bb85c537a181b0290969b4ffe7ebaafa5774
wang <wangz.today@gmail.com>
2018-07-11 10:11:25 +1000

    save a text object to s3

---
+++ node-get-started/index.js
@@ -0,0 +1,45 @@
+let AWS = require('aws-sdk');
+let uuid = require('uuid');
+
+// set the credentials
+var credentials = new AWS.SharedIniFileCredentials({ profile: 'mubaba' });
+AWS.config.credentials = credentials;
+
+// create a unique bucket name, will be display in the s3 bucket
+let bucketName = 'mubaba-node-sdk-sample' + uuid.v4();
+console.log('show the bucket Name', bucketName);
+
+bucketName = 'mubaba-node-sdk-create-sample-2018';
+// however, 我改变注意, 不论运行多少次我这个文件, 我只要生成一个 demo 的bucket
+// 后面的这个promise, 如果已经存在了这个bucket 也不会报错, 会接着运行
+
+// create name for upload object key
+let keyName = 'hello_mubaba.txt';
+
+let bucketPromise = new AWS.S3({ apiVersion: '2006-03-01' })
+  .createBucket({ Bucket: bucketName })
+  .promise();
+
+// handle promise, means we resolve the promise and do some job after then
+bucketPromise
+  .then(data => {
+    console.log('I can say I create a bucket there');
+    // create the key file and put into the bucket
+    let objectParams = {
+      Bucket: bucketName,
+      Key: keyName,
+      Body: 'Hello World!'
+    };
+    // Create object upload promise
+    let uploadPromise = new AWS.S3({ apiVersion: '2006-03-01' })
+      .putObject(objectParams)
+      .promise();
+    uploadPromise.then(data => {
+      console.log(
+        'Successfully uploaded data to ' + bucketName + '/' + keyName
+      );
+    });
+  })
+  .catch(error => {
+    console.log('error occurs', error);
+  });
